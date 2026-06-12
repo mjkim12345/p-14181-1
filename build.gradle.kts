62dda935324d562645c5e1c774bb8cd1c4a20096
user <offonoff6101@naver.com>
2026-06-12 10:49:48 +0900

    002

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,20 @@
 plugins {
     java
+<<<<<<< HEAD
     id("org.springframework.boot") version "4.1.0"
+=======
+    id("org.springframework.boot") version "4.0.6"
+>>>>>>> 5634366cf67f1afee18c4b002d28b7720d7c4059
     id("io.spring.dependency-management") version "1.1.7"
 }
 
 group = "com"
 version = "0.0.1-SNAPSHOT"
+<<<<<<< HEAD
 description = "p-14181-1"
+=======
+description = "p-14172-1"
+>>>>>>> 5634366cf67f1afee18c4b002d28b7720d7c4059
 
 java {
     toolchain {
@@ -20,10 +28,24 @@ repositories {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-webmvc")
+<<<<<<< HEAD
     compileOnly("org.projectlombok:lombok")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     annotationProcessor("org.projectlombok:lombok")
     testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
+=======
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.boot:spring-boot-h2console")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:3.0.2")
+    compileOnly("org.projectlombok:lombok")
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+    runtimeOnly("com.h2database:h2")
+    annotationProcessor("org.projectlombok:lombok")
+    testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
+    testImplementation("org.springframework.boot:spring-boot-starter-data-jpa-test")
+    testImplementation("org.springframework.boot:spring-boot-starter-validation-test")
+>>>>>>> 5634366cf67f1afee18c4b002d28b7720d7c4059
     testCompileOnly("org.projectlombok:lombok")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     testAnnotationProcessor("org.projectlombok:lombok")
